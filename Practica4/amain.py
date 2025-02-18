b2edbe8e906e0ae93d7c5c12d95bae885754b240
Uriel Martinez <122043017@upq.edu.mx>
2025-02-18 15:02:30 -0600

    Practica 4.2 Endpoints simples hechos

--- Practica4/amain.py
+++ Practica4/amain.py
@@ -4,25 +4,24 @@ from typing import List, Dict
 app = FastAPI(
     title='Administrador de Tareas',
     description='Roberto Uriel Martinez Martinez',
-    version='1.0.1'
 )
 
 tareas = [
     {
-        "id": 1, "Titulo": 
-        "Mandado", "Descripcion": 
-        "Comprar 20 Huveos, 10 manzanas", 
-        "Vencimiento" : "None", 
-        "Estado" : "Activo"
+        "id": 1, 
+        "Titulo": "Mandado", 
+        "Descripcion": "Repasar los apuntes de TAI ",
+        "Vencimiento" : "“14-02-24”", 
+        "Estado" : "Completada"
     }
 ]
 
-@app.get('/Todas_las_tareas', tags=['CRUD'])
-def showTasks():
+@app.get('/Todas_las_tareas', tags=['Opciones Tareas'])
+def mostrarTareas():
     return {"Las tareas registradas son": tareas}
 
-@app.post('/tareas/', tags=['CRUD'])
-def makeTask(tarea: Dict):
+@app.post('/tareas/', tags=['Opciones Tareas'])
+def crearTarea(tarea: Dict):
     for t in tareas:
         if t["id"] == tarea.get("id"):
             raise HTTPException(status_code=400, detail="Este id ya existe")
@@ -30,8 +29,17 @@ def makeTask(tarea: Dict):
     tareas.append(tarea)
     return tarea
 
-@app.delete('/tareas/{id}', tags=['CRUD'])
-def deleteTask(id: int):
+@app.put('/tareas/{id}', tags=['Opciones Tareas'])
+def editarTarea(id: int, TaskUpd: dict):
+    for index, t in enumerate(tareas):
+        if t["id"] == id:
+            tareas[index].update(TaskUpd) 
+            return {"mensaje": "Se actualizo la tarea correctamente", "tarea": tareas[index]}
+    
+    raise HTTPException(status_code=404, detail="No se encontro ninguna tarea")
+
+@app.delete('/tareas/{id}', tags=['Opciones Tareas'])
+def eliminarTarea(id: int):
     for index, t in enumerate(tareas):
         if t["id"] == id:
             tareas.pop(index)
