b2e0c9db7a48cb112d211c3d2f13468e4b115495
Uriel Martinez <122043017@upq.edu.mx>
2025-03-27 16:07:15 -0600

    practica 9

--- Introduccion/FAST/main.py
+++ Introduccion/FAST/main.py
@@ -82,6 +82,39 @@ def leer_usuario(id: int):
     finally:
         db.close()
 
+@app.delete("/deleteUsuario/{id}", tags=["Operaciones CRUD"])
+def eliminar_usuario(id: int):
+    db = Session()
+    try:
+        usuario = db.query(User).filter(User.id == id).first()
+        if not usuario:
+            raise HTTPException(status_code=404, detail="Usuario no encontrado")
+        db.delete(usuario)
+        db.commit()
+        return JSONResponse(content={"message": "Usuario eliminado correctamente"})
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"message": "Error al eliminar usuario", "Exception": str(e)})
+
+@app.put("/updateUsuario/{id}", response_model=Usuario, tags=["Operaciones CRUD"])
+def actualizar_usuario(id: int, usuario: Usuario):
+    db = Session()
+    try:
+        usuario_existente = db.query(User).filter(User.id == id).first()
+        if not usuario_existente:
+            return JSONResponse(status_code=404, content={"message": "Usuario no encontrado"})
+
+        for key, value in usuario.model_dump().items():
+            setattr(usuario_existente, key, value)
+
+        db.commit()
+        return JSONResponse(content={"message": "Usuario actualizado correctamente", "usuario": jsonable_encoder(usuario_existente)})
+    except Exception as e:
+        db.rollback()
+        return JSONResponse(status_code=500, content={"message": "Error al actualizar usuario", "Exception": str(e)})
+    finally:
+        db.close()
+
+
 """
 @app.put("/usuarios/{id_usuario}", response_model=Usuario, tags=["Operaciones CRUD"])
 def actualizar_usuario(id_usuario: int, usuarioActualizar: Usuario):
